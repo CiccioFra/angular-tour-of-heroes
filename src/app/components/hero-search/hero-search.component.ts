cdd67ad68a6fd96469f812bb838b89561fd24a12
Francesco <Francesco@mail>
2022-04-20 11:56:42 +0200

    Angular - affinata ricerca - FINE

--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -25,29 +25,31 @@ import { HeroService } from '../../services/hero.service';
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: [ './hero-search.component.css' ]
+  styleUrls: [ './hero-search.component.scss' ]
 })
 export class HeroSearchComponent implements OnInit {
   heroes$!: Observable<Hero[]>;
-  private searchTerms = new Subject<string>();
+  // private searchTerms = new Subject<string>(); == comment per discorso ngOnInit()
 
   constructor(private heroService: HeroService) {}
 
   // Push a search term into the observable stream.
   search(term: string): void {
-    this.searchTerms.next(term);
+    // this.searchTerms.next(term); ==> eliminato il corpo di ngOnInit() e modif.. ==>
+    this.heroes$=  this.heroService.searchHeroes(term);
   }
 
   ngOnInit(): void {
-    this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+    // TROPPO ELABORATA
+    // this.heroes$ = this.searchTerms.pipe(
+    //   // wait 300ms after each keystroke before considering the term
+    //   debounceTime(300),
 
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
+    //   // ignore new term if same as previous term
+    //   distinctUntilChanged(),
 
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.heroService.searchHeroes(term)),
-    );
+    //   // switch to new search observable each time the term changes
+    //   switchMap((term: string) => this.heroService.searchHeroes(term)),
+    // );     ==>
   }
 }
